f058dfb25f308fe414da9ef619d57bdb953874f2
Gabriel Taylor Russ <gabriel.russ@ignitionapp.com>
2025-11-04 19:13:17 +1100

    Require collate_runner

--- lib/coverage_reporter.rb
+++ lib/coverage_reporter.rb
@@ -48,6 +48,7 @@ module CoverageReporter
   require_relative "coverage_reporter/options/report"
   require_relative "coverage_reporter/pull_request"
   require_relative "coverage_reporter/report_runner"
+  require_relative "coverage_reporter/collate_runner"
   require_relative "coverage_reporter/uncovered_ranges_extractor"
   require_relative "coverage_reporter/simple_cov/patches/result_hash_formatter_patch"
 end
