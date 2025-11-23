09c05201c4d438f2a5e46c3518b7abef3dec8d8f
Gabriel Taylor Russ <gabriel.russ@ignitionapp.com>
2025-11-23 17:57:36 +1100

    puts to STDOUT

--- lib/coverage_reporter/coverage_collator.rb
+++ lib/coverage_reporter/coverage_collator.rb
@@ -54,13 +54,13 @@ module CoverageReporter
 
     def build_filter
       lambda do |src_file|
-        require "logger"
-
-        logger = Logger.new(STDOUT)
+        STDOUT.puts ">>> FILTER CALLED for #{src_file.filename}"
+        STDOUT.flush
 
         normalized_filename = src_file.filename.delete_prefix(working_dir).delete_prefix("/")
-        logger.info("Normalized filename: #{normalized_filename}")
-        logger.info("Filenames: #{filenames}")
+        STDOUT.puts "Normalized filename: #{normalized_filename}"
+        STDOUT.puts "Filenames: #{filenames.inspect}"
+        STDOUT.flush
         filenames.none?(normalized_filename)
       end
     end
